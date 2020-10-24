c1c9fcc0735c022c46157d356fa669bad13619fa
Akshara2820 <72369600+Akshara2820@users.noreply.github.com>
2020-10-24 11:13:49 +0530

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,17 +1,17 @@
       #    LEAP YEAR QUESTION #
 
 
-# s=int(input("ente the no."))
-# if s%4==0 and s%100!=0:
-#     print("leap year")
-# elif s%100==0 and s%400==0:
-#     print("leap year")
+s=int(input("ente the no."))
+if s%4==0 and s%100!=0:
+    print("leap year")
+elif s%100==0 and s%400==0:
+    print("leap year")
      
-# else:
-#     print("not leap year")
+else:
+    print("not leap year")
     
 
 
-#
+
 
 

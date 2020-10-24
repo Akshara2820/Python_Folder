62e25701eedb01957fc96defb5a25db870b9ae79
Akshara2820 <72369600+Akshara2820@users.noreply.github.com>
2020-10-24 11:16:08 +0530

    Update loop_extra.py

--- loop_extra.py
+++ loop_extra.py
@@ -1,9 +1,9 @@
-# n=int(input("enter no."))
-# s=1
-# while s<=n:
-#     if s%3==0 or s%5==0:
-#         print(s)
-#     s=s+1
+n=int(input("enter no."))
+s=1
+while s<=n:
+    if s%3==0 or s%5==0:
+        print(s)
+    s=s+1
 
 
 

61c84c9d0a09215b3cdfdf498e2d9506182147fc
Akshara2820 <72369600+Akshara2820@users.noreply.github.com>
2020-10-24 11:17:17 +0530

    Update loop_set_1.py

--- loop_set_1.py
+++ loop_set_1.py
@@ -1,173 +1,173 @@
              #  LOOP SET 1 QUESTION   #
 
-# 0st question.
+0st question.
 
-# 1 SE 100 TAKK NO. PRINT KRO
+1 SE 100 TAKK NO. PRINT KRO
 
-# a=0
-# while a<100:
-#     a=a+1
-#     print(a)
+a=0
+while a<100:
+    a=a+1
+    print(a)
     
 
 
-# 1nd question.
+1nd question.
 
 
-# 1 SE 100 TAKK NO. PRINT KRO JO 7 SE DEVIED HO.
+1 SE 100 TAKK NO. PRINT KRO JO 7 SE DEVIED HO.
 
-# a=1
-# while a<=100:
-#     if a%7==0:
-#         print(a)
-#     a=a+1
+a=1
+while a<=100:
+    if a%7==0:
+        print(a)
+    a=a+1
     
 
-# 2nd question.
+2nd question.
 
-# 1 se 100 NO. TAKK KE SUM PRINT KRO.
+1 se 100 NO. TAKK KE SUM PRINT KRO.
 
-# a=0
-# n=0
-# while a<=100:
-#     a=a+1
-#     n+=a
-#     print(n)
+a=0
+n=0
+while a<=100:
+    a=a+1
+    n+=a
+    print(n)
      
 
-#  3rd question
+ 3rd question
 
-#EK LOOP BNAOO JO USER SE 10 BAR INPUT LE.
-
-
-# a=1
-# sum=0
-# while a<=10:
-#     n=int(input("enter no. "))
-#     sum+=n
-#     a+=1
-#     print(sum)
+EK LOOP BNAOO JO USER SE 10 BAR INPUT LE.
 
 
-
-# 4th question
-
-#EK ESA CODE LIKHO JO 1,-2,3,-4 ESE PRINT KARE 100 TAKK
-
-
-# a=1
-# while a<=100:
-#     if a%2!=0:
-#         print(a*-1)
-#     else:
-#         print(a)
-#     a+=1
+a=1
+sum=0
+while a<=10:
+    n=int(input("enter no. "))
+    sum+=n
+    a+=1
+    print(sum)
 
 
 
-# EK ESAA LOOP BNAA JO 1 SE 100 TAKK PRINT KRE 
-# OR -1,2,-3,4,-5 ESE  PRINT KRO.
+4th question
+
+EK ESA CODE LIKHO JO 1,-2,3,-4 ESE PRINT KARE 100 TAKK
 
 
-# a=1
-# while a<=100:
-#     if a%2!=0:
-#         print(a*-1)
-#     else:
-#         print(a)
-#     a+=1
+a=1
+while a<=100:
+    if a%2!=0:
+        print(a*-1)
+    else:
+        print(a)
+    a+=1
 
 
 
-            #    AGAIN I AM DOING LOOP QUESTION SET.1  #
-# 0.
+EK ESAA LOOP BNAA JO 1 SE 100 TAKK PRINT KRE 
+OR -1,2,-3,4,-5 ESE  PRINT KRO.
 
-# a=1
-# while a<=100:
-#     print(a)
-#     a=a+1
 
-# 1.
+a=1
+while a<=100:
+    if a%2!=0:
+        print(a*-1)
+    else:
+        print(a)
+    a+=1
 
-# a=1
-# while a<=100:
-#      if a%7==0:
-#          print(a)
-#      a=a+1
 
-# 2.
 
-# a=0
-# n=0
-# while a<10:
-#     a=a+1
-#     n+=a
-#     print(n)
+               AGAIN I AM DOING LOOP QUESTION SET.1  #
+0.
+
+a=1
+while a<=100:
+    print(a)
+    a=a+1
+
+1.
+
+a=1
+while a<=100:
+     if a%7==0:
+         print(a)
+     a=a+1
+
+2.
+
+a=0
+n=0
+while a<10:
+    a=a+1
+    n+=a
+    print(n)
     
-# 3.
+3.
 
-# a=1
-# sum=0
-# while a<=10:
-#     n=int(input("enter the number "))
-#     sum+=n
-#     a+=1
-#     print(sum)
-
-
-
-# a=1
-# sum=0
-# while a<=10:
-#     n=int(input ("enter the no."))
-#     sum+=n
-#     a=a+1
-#     print(sum)
+a=1
+sum=0
+while a<=10:
+    n=int(input("enter the number "))
+    sum+=n
+    a+=1
+    print(sum)
 
 
 
-# a=0
-# n=0
-# while a<=100:
-#     if a%2==0:
-#         print(a*-1)
-#     else:
-#        print(a)
-#     a=a+1
+a=1
+sum=0
+while a<=10:
+    n=int(input ("enter the no."))
+    sum+=n
+    a=a+1
+    print(sum)
 
 
-# a=0
-# while a<100:
-#     a=a+1
-#     print(a)
 
-# a=1
-# n=0
-# while a<=100:
-#     if a%7==0:
+a=0
+n=0
+while a<=100:
+    if a%2==0:
+        print(a*-1)
+    else:
+       print(a)
+    a=a+1
+
+
+a=0
+while a<100:
+    a=a+1
+    print(a)
+
+a=1
+n=0
+while a<=100:
+    if a%7==0:
         
-#         print(a)
-#     a=a+1
+        print(a)
+    a=a+1
 
-# a=0
-# sum=0
-# while a<=100:
-#     a=a+1
-#     sum+=a
-#     print(sum)
-
-
+a=0
+sum=0
+while a<=100:
+    a=a+1
+    sum+=a
+    print(sum)
 
 
 
 
-# n=int(input("enter "))
-# p=len(str(n))
-# if p>=6:
-#     if n=="@" or n=="!":
-#         print("srtrong")
-#     else:
-#         print("not strong")
+
+
+n=int(input("enter "))
+p=len(str(n))
+if p>=6:
+    if n=="@" or n=="!":
+        print("srtrong")
+    else:
+        print("not strong")
 n=int(input("enter "))
 p=len(str(n))
 if n<=16:

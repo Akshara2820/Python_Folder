754fb15e8db9de070e59533e31f46b91faec9007
Akshara2820 <72369600+Akshara2820@users.noreply.github.com>
2020-10-24 11:15:12 +0530

    Update loop_Q.py

--- loop_Q.py
+++ loop_Q.py
@@ -6,105 +6,105 @@
  
  
  
- #user=int(input("enter the no. "))
-# n=0
-# i=1
-# while i<=user:
-#     a=int(input("enter the number "))
-#     n=n+a
-#     =i+1
-# print(n)
+ user=int(input("enter the no. "))
+n=0
+i=1
+while i<=user:
+    a=int(input("enter the number "))
+    n=n+a
+    =i+1
+print(n)
 
 
-# 5th question.
+5th question.
 
 
-# i = 0
-# while i <= 50:
-#   if i != 0:
-#     print(i)
-#   i = i + 5
+i = 0
+while i <= 50:
+  if i != 0:
+    print(i)
+  i = i + 5
 
 
-# 6th question
+6th question
 
-# ISNE NAA 891 SE 931 TAKK DEKHNA KI KON SA 3 SE DEVIES H .
+ISNE NAA 891 SE 931 TAKK DEKHNA KI KON SA 3 SE DEVIES H .
 
-# i = 891
-# while i < 931:
-#   z = i - 890
-#   if z % 3 == 0:
-#     print(z)
-#   i = i + 1
+i = 891
+while i < 931:
+  z = i - 890
+  if z % 3 == 0:
+    print(z)
+  i = i + 1
 
-# 7TH QUESTION
+7TH QUESTION
 
-# ISS Q ME BINA % USE KIYE 1 SE 50 TAKK K NO PRINT KRE 
-#JO 5 SE MULTIPLY HO.
-
-
-# i = 0
-# while i <= 50:
-#   if i != 0:
-#     print(i)
-#   i = i + 5
-
-#8TH QUESTION
-
-#50 SE 100 TAKK K ODD NO. PRINT
-
-# n=50
-# while n<=100:
-#     if n%2!=0:
-#         print(n)
-#     n=n+1
+ISS Q ME BINA % USE KIYE 1 SE 50 TAKK K NO PRINT KRE 
+JO 5 SE MULTIPLY HO.
 
 
-#9th QUESTION
+i = 0
+while i <= 50:
+  if i != 0:
+    print(i)
+  i = i + 5
 
-# 1 SE 10 TAKK NO. PRINT KARNA H BUT INCREMENT NHI KARNA HAI.
+8TH QUESTION
 
+50 SE 100 TAKK K ODD NO. PRINT
 
-# a=1
-# while a<=10:
-#      print(a)
-#      a=a+1
-
-# n=int(input("enter no."))
-# while n<=10:
-#     if n**2:
-#         print(n)
-#     n=n+1
-
-# 10TH  QUESTION
+n=50
+while n<=100:
+    if n%2!=0:
+        print(n)
+    n=n+1
 
 
-# FACTORE NIKALNA HAI
+9th QUESTION
+
+1 SE 10 TAKK NO. PRINT KARNA H BUT INCREMENT NHI KARNA HAI.
 
 
-# num=int(input("enter the no. "))
-# factors=[]
-# for i in range (1,num+1):
-#     if num%i==0:
-#         factors.append(i)
-# print(num,factors)
+a=1
+while a<=10:
+     print(a)
+     a=a+1
 
-# 11 th question
+n=int(input("enter no."))
+while n<=10:
+    if n**2:
+        print(n)
+    n=n+1
 
-# factore nikalna h bina for loop k or list k
+10TH  QUESTION
 
 
-# num=int(input("enter the no."))
-# factors=()
-# i=1
-# while i<=num:
-#     if num%i==0:
-#        print(i)
-#     i+=1
+FACTORE NIKALNA HAI
 
-# 12TH QUESTION .
 
-# PERFECT NO.IN FOR LOOP
+num=int(input("enter the no. "))
+factors=[]
+for i in range (1,num+1):
+    if num%i==0:
+        factors.append(i)
+print(num,factors)
+
+11 th question
+
+factore nikalna h bina for loop k or list k
+
+
+num=int(input("enter the no."))
+factors=()
+i=1
+while i<=num:
+    if num%i==0:
+       print(i)
+    i+=1
+
+12TH QUESTION .
+
+PERFECT NO.IN FOR LOOP
 
 
 n=int(input("enter the no."))

688a65463aaf6cf2af86ff30b10901a7e8da7fe6
Akshara2820 <72369600+Akshara2820@users.noreply.github.com>
2020-10-24 11:11:01 +0530

    Update if-else_set1.py

--- if-else_set1.py
+++ if-else_set1.py
@@ -1,74 +1,74 @@
-# 1st question.
+1st question.
 
-# num=int(input("enter the number."))
-# if num<=10:
-#     print("10 se chhhota hai ")
-# elif num<=20:
-#     print("10 se bada and 20 se chhota hai ")
-# else:
-#     print("20 se bada hai")
-
-
-# 2nd question.
-
-# a=int(input("enter the no."))
-# num=44*30
-# if a==num:
-#     print("equle hai ")
-# elif a<=num:
-#     print("chhota hai.")
-# else:
-#     print("bada hai")
+num=int(input("enter the number."))
+if num<=10:
+    print("10 se chhhota hai ")
+elif num<=20:
+    print("10 se bada and 20 se chhota hai ")
+else:
+    print("20 se bada hai")
 
 
-# 3rd question
+2nd question.
 
-# a=int(input("enter the no. "))
-# if a<=1:
-#     print("pani bharna hai.")
-# elif a<=10:
-#     print("pani nhi bharna hai.")
-# else:
-#     print("over flow hai")
-
-# 4th question 
-
-# a=int(input("enter the no."))
-# if a%2==0:
-#     print(a,"2 se devied hai")
-# else:
-#     print(a,"2 se devied nhi hai")
-
-# 5th question
-
-# varx=int(input("enter the no."))
-# vary=int(input("enter the no. "))
-# if varx%vary:
-#     print("devied hai")
-# else:
-#     print("not devied")
+a=int(input("enter the no."))
+num=44*30
+if a==num:
+    print("equle hai ")
+elif a<=num:
+    print("chhota ha Add files via upload i.")
+else:
+    print("bada hai")
 
 
- #6th question 
+3rd question
+
+a=int(input("enter the no. "))
+if a<=1:
+    print("pani bharna hai.")
+elif a<=10:
+    print("pani nhi bharna hai.")
+else:
+    print("over flow hai")
+
+4th question 
+
+a=int(input("enter the no."))
+if a%2==0:
+    print(a,"2 se devied hai")
+else:
+    print(a,"2 se devied nhi hai")
+
+5th question
+
+varx=int(input("enter the no."))
+vary=int(input("enter the no. "))
+if varx%vary:
+    print("devied hai")
+else:
+    print("not devied")
 
 
-# a=int(input("enter the no."))
-# b=int(input("enter the no."))
-# if a>=b:
-#     print(a)
-# else:
-#     print(b)
-
-# 7th question
-
-# num=int(input("enter the no."))
-# if num%5==0 and num%15==0:
-#     print("both devied ")
-# else:
-#     print("not devied ")
+ 6th question 
 
 
-# 8th question  
+a=int(input("enter the no."))
+b=int(input("enter the no."))
+if a>=b:
+    print(a)
+else:
+    print(b)
+
+7th question
+
+num=int(input("enter the no."))
+if num%5==0 and num%15==0:
+    print("both devied ")
+else:
+    print("not devied ")
+
+
+ 8th question  
 
 
 age=int(input("enter the age."))
